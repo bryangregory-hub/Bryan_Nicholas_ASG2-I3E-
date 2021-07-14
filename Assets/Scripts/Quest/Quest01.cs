1092396fcf02ef0d365d3dfd179c3bcf1d401ade
bryangregory-hub <s10207917@connect.np.edu.sg>
2021-07-15 03:48:20 +0800

    changes

--- Assets/Scripts/Quest/Quest01.cs
+++ Assets/Scripts/Quest/Quest01.cs
@@ -9,21 +9,14 @@ public class Quest01 : MonoBehaviour
     public GameObject ActionDisplay;
     public GameObject ActionText;
     public GameObject QuestText;
-<<<<<<< HEAD
     public GameObject QuestDetail;
-=======
-    
->>>>>>> e1ab0d216a7f47acce7de05f5a4f0fb68fbc7009
     public static bool i = false;
     
 
     void Update()
     {
         TheDistance = SamplePlayer.DistanceFromTarget;
-<<<<<<< HEAD
         
-=======
->>>>>>> e1ab0d216a7f47acce7de05f5a4f0fb68fbc7009
         Quest();
     }
      void OnMouseOver()
@@ -33,20 +26,11 @@ public class Quest01 : MonoBehaviour
             ActionDisplay.SetActive(true);
             ActionText.SetActive(true);
         }
-<<<<<<< HEAD
         
-=======
-        else
-        {
-            ActionDisplay.SetActive(false);
-            ActionText.SetActive(false);
-        }
->>>>>>> e1ab0d216a7f47acce7de05f5a4f0fb68fbc7009
         if (Input.GetButtonDown("Action"))
         {
             if (TheDistance <= 3)
             {
-<<<<<<< HEAD
                 
                 ActionDisplay.SetActive(false);
                 ActionText.SetActive(false);
@@ -57,24 +41,13 @@ public class Quest01 : MonoBehaviour
                 
             }
                 
-=======
-                ActionDisplay.SetActive(false);
-                ActionText.SetActive(false);
-                QuestText.SetActive(true);
-                i = true;
-                print("got it");
-            }
->>>>>>> e1ab0d216a7f47acce7de05f5a4f0fb68fbc7009
         }
     }
      void OnMouseExit()
     {
         ActionDisplay.SetActive(false);
         ActionText.SetActive(false);
-<<<<<<< HEAD
         QuestDetail.SetActive(false);
-=======
->>>>>>> e1ab0d216a7f47acce7de05f5a4f0fb68fbc7009
     }
     public void Quest()
     {
